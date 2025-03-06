f50e7064b0afd577e9d0fd7620df02bd69a76bc2
Samarthpkori <samarthkori47@gmail.com>
2025-03-05 22:29:07 -0800

    5/3/25

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+#INTEGERS
+a=5
+print("type of a:",type(a))
+
+#FLOAT
+b=5.0
+print("type of b:",type(b))
+
+#complex number
+c=2+3j
+print("type of c:",type(c))
+
+#BOOLEAN EXPRESSION
+print(type(True))
+print(type(False))
+
+#STRING
+string1="i am programmer"
+print(string1)
+print(type(string1))
+
+#SETS
+set1=set()
+print("initial bank set")
+print(set1)
+
+set2=set("python")
+print(set2)
